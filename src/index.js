081dee75ae5c7e71c37145147e4b1525527ab2c2
Stanislaw Chirko <post.chirko@gmail.com>
2021-02-19 10:30:41 +0100

    added impressum and datenschutz

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import './assets/sass/main.scss';
 require(`./index.html`);
 require(`./contact.html`);
+require(`./impressum.html`);
+require(`./datenschutz.html`);
+require(`./cookie-richtlinien.html`);
