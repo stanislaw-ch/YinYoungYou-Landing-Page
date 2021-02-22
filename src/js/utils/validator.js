cbbe43f759a26662b5cc9a7ed92d2b636bdd6a80
Stanislaw Chirko <post.chirko@gmail.com>
2021-02-22 21:13:24 +0100

    fixed validation

--- src/js/utils/validator.js
+++ src/js/utils/validator.js
@@ -115,10 +115,12 @@ export default class Validator {
       OK: 200
     };
 
+    // const url = action.action.replace(`/post?`, `/post-json?`) + `&c=?`;
+
     fetch(action, {
       method,
-      mode: `cors`,
-      body: form,
+      // mode: `no-cors`,
+      body: JSON.stringify(form),
     }).then((response) => {
       console.log(response.status);
       if (response.status === StatusCode.OK) {
@@ -139,3 +141,76 @@ export default class Validator {
     });
   }
 }
+
+
+// function submitMailChimp(data) {
+//   reset(data);
+//   var form = data.form;
+//   var payload = {}; // Skip Ajax submission if http/s mismatch, fallback to POST instead
+
+//   if (/^https/.test(loc.href) && !/^https/.test(data.action)) {
+//     form.attr('method', 'post');
+//     return;
+//   }
+
+//   preventDefault(data); // Find & populate all fields
+
+//   var status = findFields(form, payload);
+
+//   if (status) {
+//     return alert(status);
+//   } // Disable submit button
+
+
+//   disableBtn(data); // Use special format for MailChimp params
+
+//   var fullName;
+
+//   _.each(payload, function (value, key) {
+//     if (emailField.test(key)) {
+//       payload.EMAIL = value;
+//     }
+
+//     if (/^((full[ _-]?)?name)$/i.test(key)) {
+//       fullName = value;
+//     }
+
+//     if (/^(first[ _-]?name)$/i.test(key)) {
+//       payload.FNAME = value;
+//     }
+
+//     if (/^(last[ _-]?name)$/i.test(key)) {
+//       payload.LNAME = value;
+//     }
+//   });
+
+//   if (fullName && !payload.FNAME) {
+//     fullName = fullName.split(' ');
+//     payload.FNAME = fullName[0];
+//     payload.LNAME = payload.LNAME || fullName[1];
+//   } // Use the (undocumented) MailChimp jsonp api
+
+
+//   var url = data.action.replace('/post?', '/post-json?') + '&c=?'; // Add special param to prevent bot signups
+
+//   var userId = url.indexOf('u=') + 2;
+//   userId = url.substring(userId, url.indexOf('&', userId));
+//   var listId = url.indexOf('id=') + 3;
+//   listId = url.substring(listId, url.indexOf('&', listId));
+//   payload['b_' + userId + '_' + listId] = '';
+//   $.ajax({
+//     url: url,
+//     data: payload,
+//     dataType: 'jsonp'
+//   }).done(function (resp) {
+//     data.success = resp.result === 'success' || /already/.test(resp.msg);
+
+//     if (!data.success) {
+//       console.info('MailChimp error: ' + resp.msg);
+//     }
+
+//     afterSubmit(data);
+//   }).fail(function () {
+//     afterSubmit(data);
+//   });
+// } // Common callback which runs after all Ajax submissions
