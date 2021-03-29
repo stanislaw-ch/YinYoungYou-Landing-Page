866aaa303057349bb01c85db4224d12fa9ac0d40
Stanislaw Chirko <post.chirko@gmail.com>
2021-03-29 14:49:56 +0200

    chore: update public

---
+++ public/contact-form.php
@@ -0,0 +1,27 @@
+<?php
+if (isset($_POST)) {
+  $user_name = $_POST['user_name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+
+  $user_name = htmlspecialchars($user_name);
+  $email = htmlspecialchars($email);
+  $message = htmlspecialchars($message);
+
+  $user_name = urldecode($user_name);
+  $email = urldecode($email);
+  $message = urldecode($message);
+
+  $user_name = trim($user_name);
+  $email = trim($email);
+  $message = trim($message);
+
+  $to = "info@yin-young-you.com";
+
+  $mailSent = mail($to, "Kontakt: Neue Nachricht", "Name: ".$user_name."\r\nE-mail Adresse: ".$email."\r\nNachricht: \r\n".$message);
+
+  if ($mailSent) {
+    echo "200";
+  };
+};
+?>
---
+++ public/subscribe.php
@@ -0,0 +1,30 @@
+<?php
+if (isset($_POST)) {
+  $list_id = 'd3eb4aada4';
+  $authToken = '';
+
+  $postData = array(
+    "email_address" => $_POST["email"],
+    "status" => "pending"
+  );
+
+  $ch = curl_init('https://us7.api.mailchimp.com/3.0/lists/'.$list_id.'/members/');
+
+  curl_setopt_array($ch, array(
+    CURLOPT_POST => TRUE,
+    CURLOPT_RETURNTRANSFER => TRUE,
+    CURLOPT_HTTPHEADER => array(
+      'Authorization: apikey '.$authToken,
+      'Content-Type: application/json'
+    ),
+    CURLOPT_POSTFIELDS => json_encode($postData)
+  ));
+
+  $result = curl_exec($ch);
+  $info = curl_getinfo($ch);
+
+  // echo $response = $info["http_code"];
+  echo $result;
+};
+?>
+
